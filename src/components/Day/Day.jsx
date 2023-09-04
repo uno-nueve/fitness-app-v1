bedc189e6abbbdffc0d745ba88b03c4e07579c86
Luciano Montilla <unonueve95@gmail.com>
2023-09-04 16:33:33 -0300

    Bug fix | Components refactoring

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -4,57 +4,73 @@ import { useState } from 'react';
 
 const Day = ({dayTitle, exercisesData}) => {
     //? Exercise index state
-    const [exIndex, setExIndex] = useState(exercisesData.map(() => 0));
+    const [exIndex, setExIndex] = useState(0);
     
     //? Progress index state
-    const [progIndex, setProgIndex] = useState(exercisesData.map(() => 0));
+    const [progIndex, setProgIndex] = useState(0);
+
+    // const [exerciseStates, setExerciseStates] = useState(
+    //     exercisesData.map(() => ({exIndex: 0, progIndex: 0}))
+    // );
 
     //* Function that upgrades the exercise's progressions.
     //* Once it reaches the last progression it upgrades the exercise starting at the 1st progression.
-    const upgradeExercise = (exercise, progression, index) => {
-        if (progIndex[index] < progression[index].length - 1) {
-            setProgIndex(prevProgIndex => prevProgIndex.map(
-                (_, i) => i === index 
-                    ? progIndex[index] + 1 
-                    : _
-            ));
-        } else {
-            if (exIndex[index] < exercise.length - 1) {
-                setProgIndex(prevProgIndex => prevProgIndex.map(
-                    (_, i) => i === index 
-                        ? 0 
-                        : _
-                ));
-                setExIndex(prevExIndex => prevExIndex.map(
-                    (currentIndex, i) => i === index 
-                        ? currentIndex + 1 
-                        : currentIndex
-                ));
+
+    // const upgradeExercise = (exercise, progression, index) => {
+    //     setExerciseStates((prevStates) => {
+    //         const newStates = [...prevStates]
+    //         const currentState = newStates[index]
+
+    //         if (currentState.progIndex < progression[index][currentState.exIndex].length - 1) {
+    //             currentState.progIndex += 1
+    //         } if (currentState.progIndex === progression[index][currentState.exIndex].length - 1 && currentState.exIndex !== exercise.length - 1) {
+    //             currentState.progIndex = 0
+    //             if (currentState.exIndex < exercise.length - 1) {
+    //                 currentState.exIndex += 1
+    //             }
+    //         }
+    //         return newStates
+    //     })
+    // }
+
+    const upgradeExercise = (exercise, progression) => {
+        if (progIndex < progression[exIndex].length - 1 ) {
+            setProgIndex(progIndex + 1)
+        } if (progIndex === progression[exIndex].length - 1 && exIndex !== exercise.length - 1) {
+            setProgIndex(0)
+            if (exIndex < exercise.length - 1) {
+                setExIndex(exIndex + 1)
             }
         }
     }
     
     //* Function that downgrades the exercise's progressions.
     //* Once it reaches the 1st progression it downgrades the exercise at the last progression.
-    const downgradeExercise = (progression, index) => {
-        if (progIndex[index] > 0) {
-            setProgIndex(prevProvIndex => prevProvIndex.map(
-                (currentIndex, i) => i === index 
-                    ? currentIndex - 1 
-                    : currentIndex
-            ))
-        } else {
-            if (exIndex[index] > 0) {
-                setProgIndex(prevProvIndex => prevProvIndex.map(
-                    (_, i) => i === index 
-                        ? progression[index].length - 1 
-                        : _
-                ))
-                setExIndex(prevExIndex => prevExIndex.map(
-                    (currentIndex, i) => i === index 
-                        ? currentIndex - 1 
-                        : currentIndex
-                ))
+
+    // const downgradeExercise = (progression, index) => {
+    //     setExerciseStates((prevStates) => {
+    //         const newStates = [...prevStates]
+    //         const currentState = newStates[index]
+
+    //         if (currentState.progIndex > 0) {
+    //             currentState.progIndex -= 1
+    //         } if (currentState.progIndex === 0 && currentState.exIndex > 0) {
+    //             currentState.progIndex = progression[index][currentState.exIndex].length - 1
+    //             if (currentState.exIndex > 0) {
+    //                 currentState.exIndex -= 1
+    //             }
+    //         }
+    //         return newStates
+    //     })
+    // }
+
+    const downgradeExercise = (progression) => {
+        if (progIndex > 0) {
+            setProgIndex(progIndex - 1)
+        } if (progIndex === 0 && exIndex > 0) {
+            setProgIndex(progression[exIndex].length - 1)
+            if (exIndex > 0) {
+                setExIndex(exIndex - 1)
             }
         }
     }
@@ -67,10 +83,8 @@ const Day = ({dayTitle, exercisesData}) => {
                 {exercisesData.map(({exercise, progression}, index) => (
                     <Exercise 
                         key={index}
-                        upgrade={() => upgradeExercise(exercise, progression, index)}
-                        downgrade={() => downgradeExercise(progression, index)}
-                        current={exercise[exIndex[index]]}
-                        progress={progression[exIndex[index]][progIndex[index]]}
+                        exercise={exercise}
+                        progression={progression}
                     />
                 ))}
             </div>
@@ -78,8 +92,8 @@ const Day = ({dayTitle, exercisesData}) => {
 }
 
 Day.propTypes = {
-    dayTitle: PropTypes.string,
-    exercisesData: PropTypes.array
+    dayTitle: PropTypes.string.isRequired,
+    exercisesData: PropTypes.array.isRequired
 }
 
 export default Day;
--- src/components/Exercise/Exercise.jsx
+++ src/components/Exercise/Exercise.jsx
@@ -1,14 +1,40 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-const Exercise = ({upgrade, downgrade, current, progress}) => {
+const Exercise = ({exercise, progression}) => {
+    const [exIndex, setExIndex] = useState(0);
+    const [progIndex, setProgIndex] = useState(0);
+
+    const upgradeExercise = () => {
+        if (progIndex < progression[exIndex].length - 1 ) {
+            setProgIndex(progIndex + 1)
+        } if (progIndex === progression[exIndex].length - 1 && exIndex !== exercise.length - 1) {
+            setProgIndex(0)
+            if (exIndex < exercise.length - 1) {
+                setExIndex(exIndex + 1)
+            }
+        }
+    };
+
+    const downgradeExercise = () => {
+        if (progIndex > 0) {
+            setProgIndex(progIndex - 1)
+        } if (progIndex === 0 && exIndex > 0) {
+            setProgIndex(progression[exIndex].length - 1)
+            if (exIndex > 0) {
+                setExIndex(exIndex - 1)
+            }
+        }
+    };
+
     return  (
         <div className="day_exercise">
             <div className="exercise_title">
-                <h3>{current}</h3>
-                <p>{progress}</p>
+                <h3>{exercise[exIndex]}</h3>
+                <p>{progression[exIndex][progIndex]}</p>
                 <div className="title_buttons">
-                    <button className="upgrade-btn" onClick={upgrade}>U</button>
-                    <button className="downgrade-btn" onClick={downgrade}>D</button>
+                    <button className="upgrade-btn" onClick={upgradeExercise}>U</button>
+                    <button className="downgrade-btn" onClick={downgradeExercise}>D</button>
                 </div>
             </div>
             <div className="exercise_info">
@@ -23,10 +49,8 @@ const Exercise = ({upgrade, downgrade, current, progress}) => {
 }
 
 Exercise.propTypes = {
-    upgrade: PropTypes.func,
-    downgrade: PropTypes.func,
-    current: PropTypes.string,
-    progress: PropTypes.string
+    exercise: PropTypes.array.isRequired,
+    progression: PropTypes.array.isRequired
 }
 
 export default Exercise;
